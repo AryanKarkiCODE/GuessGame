b8c5b681384e43cdbd6008c8945914795e2a19fe
AryanKarkiCODE <aryankarki110@gmail.com>
2025-04-03 21:18:04 -0500

    added the new messages

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,5 +1,5 @@
 public class NewClass {
     public static void main (String[] args) {
-        System.out.println("This is another test just to push");
+        System.out.println("This is another test just to push"); // added comment
     }
 }
