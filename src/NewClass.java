f9f5ceaf74bb466794c95a76c200593e3e2a5ed1
AryanKarkiCODE <aryankarki110@gmail.com>
2025-04-03 21:32:57 -0500

    updated the comment

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,5 +1,6 @@
 public class NewClass {
     public static void main (String[] args) {
         System.out.println("This is another test just to push"); // added comment
+        System.out.println("This is another test just to push"); // another comment
     }
 }
